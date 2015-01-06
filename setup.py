114645ac5601323c06ba2d63cb48b6769b2fc22e
Gene Mountjoy <mountwe@live.com>
2015-01-06 18:31:00 -0500

    Initial commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup (
+	name            = 'mountwenester',
+	version         = '1.3.0',
+	py_modules      = ['mountwenester'],
+	author          = 'mountwe',
+	author_email    = 'mountwe@live.com',
+	url             = 'http://umquhile.org/mw',
+	description     = 'A simple printer of nested lists',
+	)
